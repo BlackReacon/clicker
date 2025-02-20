eda9a1cf63a48a85a00c33c3cf8f8577240667b6
PC_DARIA\black <dariaschmidt0510@gmail.com>
2025-02-20 14:06:49 +0100

    script clicks count up and generate HTML

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 /* selector */
 let numbers_clicks = document.querySelector("#image");
-let clicks = 0;
+let span_clicks = document.querySelector("#clicks");
+let clicks = 0
 
 /* eventlistener */
 numbers_clicks.addEventListener("click", countUp);
 
 /* function */
-/* count clicks */
+/* function - count clicks */
 function countUp() {
-  clicks = numbers_clicks.value + 1;
-  console.log(numbers_clicks.value);
+  clicks = clicks + 1;
+
+  generateHtml();
+}
+
+/* function - generate HTML with amount of clicks */
+function generateHtml() {
+    span_clicks.innerHTML = `${clicks}`;
 }
