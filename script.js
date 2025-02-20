9d07aa34a51109166127cf85d94fb1a3a193c11a
PC_DARIA\black <dariaschmidt0510@gmail.com>
2025-02-20 13:45:59 +0100

    link html and script, add script test function

---
+++ script.js
@@ -0,0 +1,11 @@
+/* selector */
+let numbers_clicks = document.querySelector("#image");
+
+/* eventlistener */
+numbers_clicks.addEventListener("click", countUp);
+
+/* function */
+/* count clicks */
+function countUp() {
+    console.log("test");
+}
