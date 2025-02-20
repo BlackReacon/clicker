a8de0875055d47dd2925a2188c05ccf6761e580c
PC_DARIA\black <dariaschmidt0510@gmail.com>
2025-02-20 14:42:27 +0100

    script change name, add function blink

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 /* selector */
-let numbers_clicks = document.querySelector("#image");
+let head = document.querySelector("#image");
 let span_clicks = document.querySelector("#clicks");
-let clicks = 0
+
+/* var */
+let clicks = 0;
 
 /* eventlistener */
-numbers_clicks.addEventListener("click", countUp);
+head.addEventListener("click", countUp);
 
 /* function */
 /* function - count clicks */
 function countUp() {
   clicks = clicks + 1;
 
+  blink();
+
   generateHtml();
 }
 
 /* function - generate HTML with amount of clicks */
 function generateHtml() {
-    span_clicks.innerHTML = `${clicks}`;
+  span_clicks.innerHTML = `${clicks}`;
+}
+
+/* function - blink - change img on click */
+function blink() {
+  setTimeout(() => {
+    head.src = "/src/img/Cat/Cat_1.png";
+  }, 200);
+
+  head.src = "/src/img/Cat/Cat_2.png";
 }
