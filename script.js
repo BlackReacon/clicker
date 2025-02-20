fa8d28666181becf3dfd71de9e5ac241c353d1d3
PC_DARIA\black <dariaschmidt0510@gmail.com>
2025-02-20 13:49:20 +0100

    add counter in console log

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 /* selector */
 let numbers_clicks = document.querySelector("#image");
+let clicks = 0;
 
 /* eventlistener */
 numbers_clicks.addEventListener("click", countUp);
@@ -7,5 +8,6 @@ numbers_clicks.addEventListener("click", countUp);
 /* function */
 /* count clicks */
 function countUp() {
-    console.log("test");
+  clicks = numbers_clicks.value + 1;
+  console.log(numbers_clicks.value);
 }
