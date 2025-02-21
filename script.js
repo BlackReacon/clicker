e7072ec6010dd1821622a4e5658ec46ac0e0ff97
PC_DARIA\black <dariaschmidt0510@gmail.com>
2025-02-21 08:56:12 +0100

    add script levelup counter

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 /* selector */
 let head = document.querySelector("#image");
 let span_clicks = document.querySelector("#clicks");
+let span_level = document.querySelector("#level");
 
 /* var */
 let clicks = 0;
+let level = 0;
 
 /* eventlistener */
-head.addEventListener("click", countUp);
+head.addEventListener("click", callFunction);
 
 /* function */
-/* function - count clicks */
-function countUp() {
-  clicks = clicks + 1;
+function callFunction() {
+  countUp();
 
   blink();
+
+  levelup();
 
   generateHtml();
 }
 
-/* function - generate HTML with amount of clicks */
-function generateHtml() {
-  span_clicks.innerHTML = `${clicks}`;
+/* function - count clicks */
+function countUp() {
+  clicks = clicks + 1;
 }
 
 /* function - blink - change img on click */
@@ -30,4 +33,17 @@ function blink() {
   }, 200);
 
   head.src = "/src/img/Cat/Cat_2.png";
+}
+
+/* function - level up */
+function levelup() {
+  if (clicks % 10 == 0) {
+    level = level + 1;
+  }
+}
+
+/* function - generate HTML with amount of clicks */
+function generateHtml() {
+  span_clicks.innerHTML = `${clicks}`;
+  span_level.innerHTML = `${level}`;
 }
