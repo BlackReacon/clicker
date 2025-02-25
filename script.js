ffbf3d46b428a3a13c4ffc6ee96beb28f70690f9
PC_DARIA\black <dariaschmidt0510@gmail.com>
2025-02-25 10:32:17 +0100

    add confetti, add script confetti animation, add style hero position, add index linked confetti

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let span_clicks = document.querySelector("#clicks");
 let span_level = document.querySelector("#level");
 let divProgress = document.querySelector("#progress");
 let divGrowUp = document.querySelector("#growup");
-let nextlevelprogress = document.querySelector("#nextlevel")
+let nextlevelprogress = document.querySelector("#nextlevel");
 
 /* var */
 let clicks = 0;
@@ -15,6 +15,15 @@ let clicksUntilCurrentLevel = 0;
 let clickInCurrentLevel = 0;
 let clicksUntilNextLevel = 10;
 let currentLevelClicks = 0;
+
+/* const */
+const defaults = {
+  spread: 360,
+  ticks: 100,
+  gravity: 0,
+  decay: 0.94,
+  startVelocity: 30,
+};
 
 /* array */
 eyeOpen = [
@@ -58,7 +67,6 @@ function callFunction() {
 /* function - count clicks */
 function countUp() {
   clicks = clicks + 1;
-  
 }
 
 /* function - calculate level progress and change img */
@@ -67,6 +75,7 @@ function levelProgress() {
   clickInCurrentLevel = level + 1;
   currentLevelClicks = currentLevelClicks + 1;
   if (progress >= 100) {
+    shoot();
     progress = 0;
     currentLevelClicks = 1;
     eyeIndex = Math.floor(Math.random() * eyeOpen.length);
@@ -99,5 +108,31 @@ function generateHtml() {
   span_clicks.innerHTML = `${clicks}`;
   span_level.innerHTML = `${level}`;
   divProgress.style.width = `${progress}%`;
-  nextlevelprogress.innerHTML = `<p>${currentLevelClicks} / ${clicksUntilNextLevel} next level</p>`
+  nextlevelprogress.innerHTML = `<p>${currentLevelClicks} / ${clicksUntilNextLevel} next level</p>`;
 }
+
+function shoot() {
+  confetti({
+    ...defaults,
+    particleCount: 30,
+    scalar: 1.2,
+    shapes: ["circle", "square"],
+    colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"],
+  });
+
+  confetti({
+    ...defaults,
+    particleCount: 20,
+    scalar: 2,
+    shapes: ["emoji"],
+    shapeOptions: {
+      emoji: {
+        value: ["🦄", "🌈"],
+      },
+    },
+  });
+}
+
+setTimeout(shoot, 0);
+setTimeout(shoot, 100);
+setTimeout(shoot, 200);
